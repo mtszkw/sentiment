631eefb30b87ba91c560bf51e4d4bb1ad147823a
Mateusz Kwasniak <mateusz.kwasniak@pm.me>
2021-10-25 14:14:59 +0200

    Fix training flow

--- execute.sh
+++ execute.sh
@@ -1,4 +1,9 @@
 python3 -m venv ./env
+
 . ./env/bin/activate
+
 python3 -m pip install -r model/requirements.txt --quiet
-python3 model/training_flow.py run --data_path=data/training.1600000.processed.noemoticon.csv
+
+python3 model/training_flow.py run \
+  --data_path=data/training.1600000.processed.noemoticon.csv \
+  --quickrun_pct=0.001
--- model/training_flow.py
+++ model/training_flow.py
@@ -1,7 +1,7 @@
 from metaflow import FlowSpec, Parameter, step
 import pandas as pd
 import scipy
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from sklearn.model_selection import train_test_split
 import torch
 
@@ -14,6 +14,7 @@ class TrainingFlow(FlowSpec):
     data_path = Parameter('data_path', help='Path to CSV file with input data', required=True)
     rnd_seed = Parameter('rnd_seed', help='Seed for Random Number Generator', default=42)
     test_size = Parameter('test_size', help='Size of test set for train-test split (in %)', default=0.1)
+    quickrun_pct = Parameter('quickrun_pct', help='% of data to use for quick run (e.g. 0.1, 1 to use all)', default=1.0)
 
     @step
     def start(self):
@@ -24,8 +25,13 @@ class TrainingFlow(FlowSpec):
         print(f'Downloading data from {self.data_path}')
         try:
             data_columns  = ["sentiment", "ids", "date", "flag", "user", "text"]
-            self.df_data_raw = pd.read_csv(self.data_path, names=data_columns)
-            print(self.df_data_raw.head())
+            skip_every_nthline = int(1/self.quickrun_pct) if self.quickrun_pct < 1.0 else 1
+            print(f"Skipping every {skip_every_nthline}th line thus reading {self.quickrun_pct*100}% of data")
+            self.df_raw = pd.read_csv(
+                self.data_path,
+                names=data_columns,
+                skiprows=lambda i: i % skip_every_nthline != 0)
+            print(f"Reading {self.df_raw.shape} data frame from {self.data_path}")
         except Exception as exc:
             print(f"Exception: {exc}")
         self.next(self.process_data)
@@ -34,7 +40,7 @@ class TrainingFlow(FlowSpec):
     def process_data(self):
         data_proc = Sentiment140Preprocessing()
         self.df_text, self.df_sentiment = data_proc(
-            self.df_data_raw,
+            self.df_raw,
             target='sentiment')
         self.next(self.train_test_split)
     
@@ -45,41 +51,27 @@ class TrainingFlow(FlowSpec):
             self.df_sentiment,
             test_size=self.test_size,
             random_state=self.rnd_seed)
-        print(f"Train set type: f{type(self.X_train)}")
-        print(f"Train set size: {self.X_train.shape}, test set: {self.X_test.shape}")
+        print(f"Train set size: [{self.X_train.shape}, {self.y_train.shape}], test set: [{self.X_test.shape}, {self.y_test.shape}]")
         self.next(self.tfidf_vectorization)
 
     @step
     def tfidf_vectorization(self):
         tfidf = TfIdfDataVectorizer()
         self.X_train, self.X_test = tfidf(self.X_train, self.X_test)
-        print(f"Train set type: f{type(self.X_train)}")
         print(f"Train set size: {self.X_train.shape}, test set: {self.X_test.shape}")
-        self.next(self.train_linearsvc, self.train_mlp)
+        self.next(self.train_linearsvc)
 
     @step
     def train_linearsvc(self):
         svc_model = LinearSVCModel()
         svc_model.train(self.X_train, self.y_train)
-        self.y_pred = svc_model.predict(self.X_test)
+        self.linearsvc_y_pred = svc_model.predict(self.X_test)
+        print(f"Linear SVC accuracy on test set = {accuracy_score(self.y_test, self.linearsvc_y_pred)}")
         self.next(self.visualize_results)
 
     @step
-    def train_mlp(self):
-        mlp_model = MLPModel(
-            input_size=self.X_train.shape[1],
-            hidden_sizes=[1024, 512])
-        X_train_tensor = torch.Tensor(scipy.sparse.csr_matrix.todense(self.X_train)).float()
-        y_train_tensor = torch.Tensor(self.y_train.values)
-        X_test_tensor = torch.Tensor(scipy.sparse.csr_matrix.todense(self.X_test)).float()
-        mlp_model.train(X_train_tensor, y_train_tensor)
-        self.y_pred = mlp_model.predict(X_test_tensor)
-        self.next(self.visualize_results)
-
-    @step
-    def visualize_results(self, inputs):
-        print(f"LinearSVC:\n{classification_report(self.y_test, inputs.train_linearsvc.y_pred)}")
-        print(f"Torch MLP:\n{classification_report(self.y_test, inputs.train_mlp.y_pred)}")
+    def visualize_results(self):
+        print(f"LinearSVC:\n{classification_report(self.y_test, self.linearsvc_y_pred)}")
         self.next(self.end)
 
     @step

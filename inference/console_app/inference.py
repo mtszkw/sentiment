bbdba234cb14d70bb9826953835a2d840b145890
Mateusz Kwasniak <mateusz.kwasniak@pm.me>
2021-12-18 15:46:56 +0100

    Add console app for inference

---
+++ inference/console_app/inference.py
@@ -0,0 +1,31 @@
+import argparse
+import joblib
+
+# from sklearn.svm import LinearSVC
+from pipeline import TextPreprocessing
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_joblib_path", help="Path to the serialized model")
+    parser.add_argument("vectorizer_joblib_path", help="Path to the serialized vectorizer")
+    parser.add_argument("input_text", help="Input text to classify")
+
+    args = parser.parse_args()
+    print(f"Arguments: {args}")
+
+    clf = joblib.load(args.model_joblib_path)
+    vectorizer = joblib.load(args.vectorizer_joblib_path)
+
+    # Preprocessing
+    input_text = args.input_text
+    preprocessor = TextPreprocessing()
+    map(preprocessor.convert_to_lowercase, input_text)
+    map(preprocessor.remove_stopwords, input_text)
+    map(preprocessor.replace_regex_patterns, input_text)
+    print(f"Input text after preprocessing: {input_text}")
+
+    text_embeddings = vectorizer.transform(input_text)
+    print(f"Text embeddings: {text_embeddings}")
+
+    output = clf.predict(text_embeddings)
+    print(f"Model output: {output}")
--- infra/var.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-default = "XXX"
-}
-variable "aws_secret_key" {
-default = "YYY"
- }
-variable "region" {
-    default = "eu-central-1"
-}
